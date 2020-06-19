fb3dfa79be8b8ba425c8c7977c37f88ecbdaaac8
jatinpanday <pandajatin98@gmail.com>
2020-06-20 01:23:09 +0530

    My-commit

---
+++ src/build/precache-manifest.5d01e21b7f451396f22e01ca4c861880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562958269bfb92a28e6d67b076c7d701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ba2073f6c8e6228c928",
+    "url": "/static/css/main.91f85918.chunk.css"
+  },
+  {
+    "revision": "26041430bee89349bd79",
+    "url": "/static/js/2.86710a4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.86710a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba2073f6c8e6228c928",
+    "url": "/static/js/main.3c00ffe5.chunk.js"
+  },
+  {
+    "revision": "51ff1df8d02dd786cb92",
+    "url": "/static/js/runtime-main.3ce2ee87.js"
+  }
+]);
